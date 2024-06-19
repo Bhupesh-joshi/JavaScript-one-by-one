8c781a7aa9ff079117a1f118d60df3e8fff82fdd
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-06-19 07:31:04 +0530

    q-25 done

---
+++ q-25.js
@@ -0,0 +1,36 @@
+// Arrays
+
+let fruits = ['apple', 'banana', 'mango'];
+
+console.log(fruits[0]); // 'apple'
+console.log(fruits[1]); // 'banana'
+console.log(fruits[2]); // 'mango'
+
+
+// single dimensional Array
+// multi dimensional Array 
+
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+
+// dynamic in size
+let fruits = ['apple', 'banana'];
+fruits.push('mango');
+console.log(fruits); // ['apple', 'banana', 'mango']
+fruits.pop();
+console.log(fruits); // ['apple', 'banana']
+
+
+
+let mixedArray = [1, 'apple', true];
+console.log(mixedArray); // [1, 'apple', true]
+
+
+forEach
+map
+filter
