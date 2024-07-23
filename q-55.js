7c26eac149f8fdd37ed4082fabbaf3f11703a67e
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-07-23 21:41:37 +0530

    q-55 rest parameter vs arg obj again

--- q-55.js
+++ q-55.js
@@ -9,5 +9,5 @@ function newStyleSum(...numbers) {
     return numbers.reduce((total, num) => total + num, 0);
 }
 
-console.log(oldStyleSum(1, 2, 3));
+// console.log(oldStyleSum(1, 2, 3));
 console.log(newStyleSum(1, 2, 3));
