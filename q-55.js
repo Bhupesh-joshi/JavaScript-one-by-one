c047e03965537dce354f180019dec4538aea534e
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-07-23 21:40:23 +0530

    q-55 rest parameter vs arg obj done

---
+++ q-55.js
@@ -0,0 +1,13 @@
+// Differences between Rest Parameters and Arguments Object
+
+function oldStyleSum() {
+    const args = Array.prototype.slice.call(arguments);
+    return args.reduce((total, num) => total + num, 0);
+}
+
+function newStyleSum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+}
+
+console.log(oldStyleSum(1, 2, 3));
+console.log(newStyleSum(1, 2, 3));
