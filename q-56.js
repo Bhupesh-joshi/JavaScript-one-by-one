6f41afaea22a7abc298011c202615385c1dbd5d4
Bhupesh Joshi <78612301+Bhupesh-joshi@users.noreply.github.com>
2024-07-24 20:14:31 +0530

    q-56 proxie and reflection 1 done

---
+++ q-56.js
@@ -0,0 +1,16 @@
+// Proxies aur Reflection
+
+const target = {
+    message: "Hello, world!"
+};
+
+const handler = {
+    get: function(target, property) {
+        return property in target ? target[property] : `Property ${property} does not exist.`;
+    }
+};
+
+const proxy = new Proxy(target, handler);
+
+console.log(proxy.message);
+console.log(proxy.nonExistentProp);
